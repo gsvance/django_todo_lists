0cf768d39d12a6b54bf9bf4688d8beda2802efe7
Gregory Vance <13666175+gsvance@users.noreply.github.com>
2026-02-02 17:19:32 -0500

    Complete Step 5 of the Real Python article

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,4 +1,7 @@
-# todo_app/urls.py
+from django.urls import path
+from . import views
 
-urlpatterns = [    
+urlpatterns = [
+    path('', views.ListListView.as_view(), name='index'),
+    path('list/<int:list_id>/', views.ItemListView.as_view(), name='list'),
 ]
